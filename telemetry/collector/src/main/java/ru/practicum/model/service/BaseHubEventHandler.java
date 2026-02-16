1b3cc3cc4ee95b3f2144fa1568949f48f6b8d2a1
AndreyAA-code <otd53@yandex.ru>
2026-02-16 22:31:32 +0300

    added specification.json added sensor events added hub events added sensor and hub avro

---
+++ telemetry/collector/src/main/java/ru/practicum/model/service/BaseHubEventHandler.java
@@ -0,0 +1,45 @@
+package ru.practicum.model.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import ru.practicum.model.hub_event.HubEvent;
+
+@Slf4j
+@RequiredArgsConstructor
+public abstract class BaseHubEventHandler<T extends SpecificRecordBase> implements HubEventHandler {
+    protected final KafkaClientProducer producer;
+
+    @Value("${kafka.topic.hub}")
+    protected String topic;
+
+    protected abstract T mapToAvro(HubEvent event);
+
+    @Override
+    public void handle(HubEvent event) {
+        if (!event.getType().equals(getMessageType())) {
+            throw new IllegalArgumentException("Неизвестный тип события: " + event.getType());
+        }
+
+        T payload = mapToAvro(event);
+
+        HubEventAvro eventAvro = HubEventAvro.newBuilder()
+                .setHubId(event.getHubId())
+                .setTimestamp(event.getTimestamp())
+                .setPayload(payload)
+                .build();
+
+        ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(
+                topic,
+                null,
+                event.getTimestamp().toEpochMilli(),
+                eventAvro.getHubId(),
+                eventAvro);
+
+        producer.getProducer().send(record);
+
+        log.info("Отправлено в Kafka: {}", record);
+    }
+}
