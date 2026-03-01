4c1532697769680e8536fc72fbd00e3208d6514e
AndreyAA-code <otd53@yandex.ru>
2026-03-01 14:45:00 +0300

    added hub_event.proto and sensor_event.proto and collector_controller.proto hubrouter module added

---
+++ telemetry/hubrouter/src/main/java/ru/yandex/practicum/EventDataProducer.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum;
+
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.grpc.telemetry.collector.CollectorControllerGrpc;
+
+@Component
+public class EventDataProducer {
+
+   @GrpcClient("collector")
+   private CollectorControllerGrpc.CollectorControllerBlockingStub collectorStub;
+
+}
