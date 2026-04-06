97c90f6495e017c606aca7ee5a3035d825d657fa
AndreyAA-code <otd53@yandex.ru>
2026-04-06 15:16:25 +0300

    order model

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/api/OrderFeignClient.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/api/OrderFeignClient.java
@@ -1,8 +1,52 @@
 package ru.yandex.practicum.api;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.dto.order.CreateNewOrderRequest;
+import ru.yandex.practicum.dto.order.OrderDto;
+import ru.yandex.practicum.dto.order.ProductReturnRequest;
+
+import java.util.List;
+import java.util.UUID;
 
 @FeignClient(name = "order", path = "/api/v1/order")
 public interface OrderFeignClient {
+
+    @GetMapping
+    List<OrderDto> getOrders(@RequestParam(name = "username") @NotNull String username);
+
+    @PutMapping
+    OrderDto createNewOrder(@RequestBody CreateNewOrderRequest orderRequest);
+
+    @PostMapping("/return")
+    OrderDto returnOrder(@RequestBody ProductReturnRequest returnRequest);
+
+    @PostMapping("/payment")
+    OrderDto payment(@RequestBody UUID orderId);
+
+    @PostMapping("/payment/failed")
+    OrderDto paymentFailed(@RequestBody @NotNull UUID orderId);
+
+    @PostMapping("/delivery")
+    OrderDto delivery(@RequestBody @NotNull UUID orderId);
+
+    @PostMapping("/delivery/failed")
+    OrderDto deliveryFailed(@RequestBody @NotNull UUID orderId);
+
+    @PostMapping("/completed")
+    OrderDto complete(@RequestBody @NotNull UUID orderId);
+
+    @PostMapping("/calculate/total")
+    OrderDto calculateTotalCost(@RequestBody @NotNull UUID orderId);
+
+    @PostMapping("/calculate/delivery")
+    OrderDto calculateDeliveryCost(@RequestBody @NotNull UUID orderId);
+
+    @PostMapping("/assembly")
+    OrderDto assembly(@RequestBody @NotNull UUID orderId);
+
+    @PostMapping("/assembly/failed")
+    OrderDto assemblyFailed(@RequestBody @NotNull UUID orderId);
 
 }
--- commerce/order/src/main/java/ru/yandex/practicum/model/Order.java
+++ commerce/order/src/main/java/ru/yandex/practicum/model/Order.java
@@ -1,13 +1,17 @@
 package ru.yandex.practicum.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import ru.yandex.practicum.dto.order.OrderState;
 
+import java.math.BigDecimal;
+import java.util.Map;
 import java.util.UUID;
 
+@Entity
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table (name = "order", schema = "order")
@@ -16,8 +20,30 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "order_id")
     private UUID orderId;
+    @Column(name = "shopping_cart_id", nullable = true)
+    private UUID shoppingCartId;
+    @ElementCollection
+    @CollectionTable(name = "order_products", joinColumns = @JoinColumn(name = "order_id"))
+    @MapKeyColumn(name = "product_id")
+    @Column(name = "quantity")
+    private Map<UUID, Integer> products;
     @Column(name = "payment_id")
     private UUID paymentId;
-
-
+    @Column(name = "delivery_id")
+    private UUID deliveryId;
+    @Column(name = "order_state", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OrderState orderState = OrderState.NEW;
+    @Column(name = "delivery_weight")
+    private Double deliveryWeight;
+    @Column(name = "delivery_volume")
+    private Double deliveryVolume;
+    @Column(name = "fragile")
+    private Boolean fragile;
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+    @Column(name = "delivery_price")
+    private BigDecimal deliveryPrice;
+    @Column(name = "product_price")
+    private BigDecimal productPrice;
 }
