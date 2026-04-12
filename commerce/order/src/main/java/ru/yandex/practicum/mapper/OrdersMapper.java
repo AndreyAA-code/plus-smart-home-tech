67177ba4387b6c227d9892697205d57f9a5284e2
AndreyAA-code <otd53@yandex.ru>
2026-04-12 14:03:30 +0300

    started with order.json schema, config dixcovery feign structure payment structure deliveru structure added gateway routes adding methods to order Service

--- commerce/order/src/main/java/ru/yandex/practicum/mapper/OrdersMapper.java
+++ commerce/order/src/main/java/ru/yandex/practicum/mapper/OrdersMapper.java
@@ -14,19 +14,19 @@ import ru.yandex.practicum.model.Orders;
 public interface OrdersMapper {
 
     @Mapping(target = "orderId", ignore = true)
-    @Mapping(target = "products", expression = "java(orderRequest.getShoppingCart().getProducts())")
-    @Mapping(target = "shoppingCartId", source = "orderRequest.shoppingCart.shoppingCartId")
+    @Mapping(target = "products", expression = "java(request.getShoppingCart().getProducts())")
+    @Mapping(target = "shoppingCartId", source = "request.shoppingCart.shoppingCartId")
     @Mapping(target = "deliveryWeight", source = "bookedProductsDto.deliveryWeight")
     @Mapping(target = "deliveryVolume", source = "bookedProductsDto.deliveryVolume")
     @Mapping(target = "fragile", source = "bookedProductsDto.fragile")
-    @Mapping(target = "username", source = "orderRequest.username")
+    @Mapping(target = "username", source = "request.username")
     @Mapping(target = "paymentId", ignore = true)
     @Mapping(target = "deliveryId", ignore = true)
-    @Mapping(target = "orderState", ignore = true)
+    @Mapping(target = "orderState", constant = "NEW")
     @Mapping(target = "totalPrice", ignore = true)
     @Mapping(target = "deliveryPrice", ignore = true)
     @Mapping(target = "productPrice", ignore = true)
-    Orders toOrders(CreateNewOrderRequest orderRequest, BookedProductsDto bookedProductsDto);
+    Orders toOrders(CreateNewOrderRequest request, BookedProductsDto bookedProductsDto);
 
     OrdersDto toOrdersDto(Orders orders);
 }
